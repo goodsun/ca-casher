ecba96106267a45b14a11391f4eb4c234c13fdb5
goodsun <goodsun0317@gmail.com>
2025-06-30 09:53:44 +0900

    Lambda update

--- cdk/lambda/handler.ts
+++ cdk/lambda/handler.ts
@@ -3,11 +3,10 @@ import app from './app';
 
 export const handler = serverlessExpress({ 
   app,
-  stripBasePath: true,
   binaryMimeTypes: [
-    'application/json',
     'application/octet-stream',
-    'text/html',
-    'text/plain'
+    'image/*',
+    'audio/*',
+    'video/*'
   ]
 });
